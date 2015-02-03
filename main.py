880441a2acc7076dfe69d913652dbe3dc18a250e
Max Vilimpoc <maxv@hopstash.beer>
2015-02-03 22:32:41 +0000

    Initial commit from local side.

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/get/data')
+def index():
+    return render_template('index.html')
+
+
+SENSOR_MAPPING = {
+}
+
+class RealWorldData:
+    def __init__(self):
+        pass
+
+class RealWorldIo:
+    def __init__(self, real_world_data):
+        pass
+
+class DataBus:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+    data_bus = DataBus()
